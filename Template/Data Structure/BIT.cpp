f94825e8c9c36217cea3e023bd375a7670aae7b2
Mirror2529156651 <42204820+Mirror2529156651@users.noreply.github.com>
2019-11-03 15:39:11 +0800

    Add files via upload

--- Template/Data Structure/BIT.cpp
+++ Template/Data Structure/BIT.cpp
@@ -1,20 +1,23 @@
 struct BIT
 {
     vector<int> v;
+
     BIT(int n)
     {
         v.resize(n);
     }
+
     int query(int x)
     {
         int r=0;
         for(;x;x-=lowbit(x))
-            r=max(r,v[x]);
+            r+=v[x];
         return r;
     }
+    
     void modify(int x,int val)
     {
         for(;x<v.size();x+=lowbit(x))
-            v[x]=max(v[x],val);
+            v[x]+=val;
     }
 };
